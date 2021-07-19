81555acb120ad71cb89fabac64c72cdf2f292de6
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-19 12:26:00 +0000

    [Update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'homes#top'
   devise_for :users
+  root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   resources :users, only: [:show, :index, :edit, :update]
