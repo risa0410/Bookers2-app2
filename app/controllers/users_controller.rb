64655be8d3388cc7179c0d322e7d74fd874c208c
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-20 14:37:00 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image, :int)
+    params.require(:user).permit(:name, :image, :introduction)
   end
   
 end
