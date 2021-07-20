af30aba7208dd7abec06e1117f416e4d5a9cbe24
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-20 01:02:23 +0000

    [Update]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   
+  def show
+    @User = User.find(params[:id])
+  end
+  
 end
