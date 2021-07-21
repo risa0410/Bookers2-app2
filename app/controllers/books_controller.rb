c821170a144ec97ba6e3158dc6b488c612411137
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-21 07:17:56 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(params[:id])
+   book = Book.new(book_params)
+   book.save
+   redirect_to user_path(@user.id)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
   
 end
