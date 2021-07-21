8a3366f6255ce1ec786959a2c91d6653fc1893c8
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-21 05:34:57 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
   
-  def new
-    @book = Book.new
+  def index
+   @user = current_user
+   @books = Book.all
+   @book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
     @book.save
     redirect_to book_path(params[:id])
+  end
+  
+end
