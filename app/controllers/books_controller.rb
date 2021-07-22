9cc9cfb147a4b79924e21b1d07f7654b41e31b33
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-22 13:27:59 +0000

    [update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
    @book.user_id = current_user.id
    @book.save
    redirect_to book_path(@book)
+   flash[:notice] = "You have created book successfully."
   end
   
   def show
@@ -27,6 +28,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book)
+    flash[:notice] = "You have updated book successfully."
   end
   
   def destroy
