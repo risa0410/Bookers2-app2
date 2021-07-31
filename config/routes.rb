5afa84b8798fc58c76a758cb00878aa8814e67af
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-31 14:40:03 +0000

    [Update]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   resources :users, only: [:new, :show, :index, :edit, :update, :destroy]
+  resource :favorites, only: [:create, :destroy]
   get "/home/about" => "homes#about"
 
 end
