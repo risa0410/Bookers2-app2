b1ef426703967bb57b097957d0d5e3ba0be2ab7b
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-01 12:15:13 +0000

    [Update]コメント機能あとちょっと

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -14,7 +14,7 @@ class BookCommentsController < ApplicationController
   private
 
   def book_comment_params
-    params.require(:book).permit(:comment)
+    params.require(:book_comment).permit(:comment)
   end
 
 end
