43cfeba00a65671340c4274afac1a106c946296d
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-02 08:37:09 +0000

    [Update]フォロー機能作成中

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'homes#top'
   get "/home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :show, :index, :edit, :update, :destroy]
+  resources :users, only: [:new, :show, :index, :edit, :update, :destroy] do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
