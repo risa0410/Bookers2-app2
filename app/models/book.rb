8877488e5832dd95bd2ada6609ef276b21e416c2
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-09 08:52:59 +0000

    [Check]

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,9 +5,9 @@ has_many :favorites, dependent: :destroy
 has_many :favorited_users, through: :favorites, source: :user
 has_many :book_comments, dependent: :destroy
 
-  # def favorited_by?(user)
-  #   favorites.where(user_id: user.id).exists?
-  # end
+def favorited_by?(user)
+  favorites.where(user_id: user.id).exists?
+end
 
 validates :title, presence: true
 validates :body, presence: true, length: { maximum: 200 }
---
+++ db/migrate/20210809032725_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.integer :id
+      t.refarence :user
+      t.refarence :room
+
+      t.timestamps
+    end
+  end
+end
